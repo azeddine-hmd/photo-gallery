87f9e9e94812bd47667c0b7f9cc60c82c81d3bc4
azeddine <azeddine.hmd@gmail.com>
2020-09-20 19:06:53 +0100

    Initial commit

---
+++ app/src/main/java/com/innocent/learn/photogallery/PhotoGalleryFragment.kt
@@ -0,0 +1,24 @@
+package com.innocent.learn.photogallery
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class PhotoGalleryFragment : Fragment() {
+    private lateinit var recyclerView: RecyclerView
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_photo_gallery, container, false)
+        recyclerView = view.findViewById(R.id.photo_recycler_view)
+        recyclerView.layoutManager = GridLayoutManager(activity, 3)
+        return view
+    }
+}
